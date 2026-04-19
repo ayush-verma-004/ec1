ddf42db9e3cdf97b2a1daa05e6db59f7a5e883c7
ayush-verma-004 <ayushverma593773@gmail.com>
2026-04-19 17:36:11 +0530

    chore: Complete removal of Hazelcast in favor of Spring Simple Cache

--- backend/src/main/java/com/javnic/econe/EconeApplication.java
+++ backend/src/main/java/com/javnic/econe/EconeApplication.java
@@ -2,10 +2,12 @@ package com.javnic.econe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
 @EnableAsync
 @EnableScheduling
 public class EconeApplication {
